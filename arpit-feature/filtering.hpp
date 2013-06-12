50c5d8e8b33795938f60ebc9d50ddce466ac62b8
anjan Sarkar <anjan@anjan-desktop>
2013-06-12 18:42:44 +0530

    inverse distance interpolation implemented. works pretty well, but need to make boundaries continuous.

--- arpit-feature/filtering.hpp
+++ arpit-feature/filtering.hpp
@@ -59,6 +59,108 @@ namespace Filter {
     merge(planes, block);
     return ret;
   }
+  /// block1, block2: short
+  /// mask: uchar
+  Point getCentroid(Mat block1, Mat block2, Mat mask, Point mean, double sdI, double sdJ) {
+    assert(block1.size() == block2.size());
+    double sumi=0, sumj=0;
+    int count = 0;
+    for (int i = 0; i < block1.rows; ++i)
+    {
+      for (int j = 0; j < block1.cols; ++j)
+      {
+        if(!mask.at<uchar>(i,j))
+          continue;
+        short &vi = block1.at<short>(i,j);
+        short &vj = block2.at<short>(i,j);
+        if((vi-mean.y)*(vi-mean.y)/(sdI*sdI) - (vj-mean.x)*(vj-mean.x)/(sdJ*sdJ) <= 1) {
+          sumi += i;
+          sumj += j;
+          count++;
+        }
+      }
+    }
+    if(count) {
+      sumi /= count;
+      sumj /= count;
+    }
+    else {
+      printf("count 0!!\n");
+    }
+    Size size; Point ofs;
+    block1.locateROI(size, ofs);
+    return Point(sumj+ofs.x, sumi+ofs.y);
+  }
+  double distSq(DisparityElemType p1, DisparityElemType p2) {
+    return (p1[0]-(double)p2[0])*(p1[0]-p2[0])+(p1[1]-(double)p2[1])*(p1[1]-p2[1]);
+  }
+  /// points: dim=2; ch1: i, ch2: j
+  /// values: dim=2; ch1: deli, ch2: delj
+  /// both are CV_16SC2
+  Mat interpolate(Mat points, Mat values, int blockSize) {
+    assert(points.size() == values.size());
+    static int neighbours[][2] = { {-1,-1},
+              {-1, 0},
+              {-1, 1},
+              {0, -1},
+              {0, +1},
+              {1, -1},
+              {+1, 0},
+              {+1, 1}};
+    Mat result = Mat::zeros(Size(points.cols*blockSize, points.rows*blockSize), CV_16SC2);
+    for (int i = 0; i < points.rows; ++i)
+    {
+      for (int j = 0; j < points.cols; ++j)
+      {
+        DisparityElemType centroid = points.at<DisparityElemType>(i,j);
+        DisparityElemType value = values.at<DisparityElemType>(i,j);
+        vector<DisparityElemType> nbCentroids;
+        vector<DisparityElemType> nbValues;
+        nbCentroids.push_back(centroid);
+        nbValues.push_back(value);
+        for(int k=0; k<8; k++)
+        {
+          int ni = i+neighbours[k][0];
+          int nj = j+neighbours[k][1];
+          if(ni < 0 || ni >= points.rows || nj < 0 || nj >= points.cols)
+            continue;
+          nbCentroids.push_back(points.at<DisparityElemType>(ni,nj));
+          nbValues.push_back(values.at<DisparityElemType>(ni,nj));
+        }
+        for (int ii = i*blockSize; ii < blockSize+i*blockSize; ++ii)
+        {
+          for (int jj = j*blockSize; jj < blockSize+j*blockSize; ++jj)
+          {
+            if(ii == centroid[0] && jj == centroid[1]) {
+              result.at<DisparityElemType>(ii, jj) = value;
+              continue;
+            }
+            double wtSum = 0;
+            double vali = 0, valj=0;
+            DisparityElemType currP(ii, jj);
+            for (int l = 0; l < nbCentroids.size(); ++l)
+            {
+              double wt = distSq(currP, nbCentroids[l]);
+              if(wt == 0.0) {
+                printf("ii jj: %d %d, centroid: %d %d, nbCentroids[%d]= %d %d\n", (int)ii, (int)jj, (int)centroid[0], (int)centroid[1], (int)l, (int)nbCentroids[l][0], (int)nbCentroids[l][1]);
+                assert(0);
+              }
+              assert(wt != 0.0);
+              wt = (1/wt);
+              wtSum += wt;
+              vali += wt * nbValues[l][0]; 
+              valj += wt * nbValues[l][1];
+            }
+            assert(wtSum != 0);
+            vali /= wtSum;
+            valj /= wtSum;
+            result.at<DisparityElemType>(ii, jj) = DisparityElemType(vali, valj);
+          }
+        }
+      }
+    }
+    return result;
+  }
   /// applies getModalPoint, getWrongPoints and interpolation on src1 and src2
   /// input images should not be blocky!
   /// blockSize is for region to consider for modal evaluation
@@ -66,7 +168,9 @@ namespace Filter {
   Mat applyModalFilter(Mat mask, Mat src1, Mat src2, int blockSize) {
     assert(src1.size() == src2.size() && mask.size() == src1.size());
     assert(src1.rows >= blockSize && src1.cols >= blockSize);
-    Mat res = Mat(Size(src1.cols/blockSize, src1.rows/blockSize), CV_16SC2);
+    // Mat res = Mat(Size(src1.cols/blockSize, src1.rows/blockSize), CV_16SC2);
+    Mat centroids = Mat(Size(src1.cols/blockSize, src1.rows/blockSize), CV_16SC2);
+    Mat values = Mat(centroids.size(), CV_16SC2);
     for (int i = 0; i < src1.rows-blockSize+1; i += blockSize)
     {
       for (int j = 0; j < src1.cols-blockSize+1; j += blockSize)
@@ -82,14 +186,23 @@ namespace Filter {
         roi_src1.convertTo(b1, CV_32FC1);
         roi_src2.convertTo(b2, CV_32FC1);
         Point p = getModalPoint(b1, b2, roi_mask, NULL);
-        int &dispI = p.y;
-        int &dispJ = p.x;
-        /// Right now not finding centroid. Using the centre of the block itself for interpolation.
-        res.at<DisparityElemType>(i/blockSize, j/blockSize)[0] = dispI;
-        res.at<DisparityElemType>(i/blockSize, j/blockSize)[1] = dispJ;
+        Point cent = getCentroid(roi_src1, roi_src2, roi_mask, p, 5, 2);
+        centroids.at<DisparityElemType>(i/blockSize,j/blockSize) = DisparityElemType((short)cent.y, (short)cent.x);
+        values.at<DisparityElemType>(i/blockSize,j/blockSize) = DisparityElemType((short)p.y, (short)p.x);
+        // int &dispI = p.y;
+        // int &dispJ = p.x;
+        // /// Right now not finding centroid. Using the centre of the block itself for interpolation.
+        // res.at<DisparityElemType>(i/blockSize, j/blockSize)[0] = dispI;
+        // res.at<DisparityElemType>(i/blockSize, j/blockSize)[1] = dispJ;
       }
     }
-    resize(res, res, src1.size(), 0, 0, INTER_LINEAR);
+    // cout << centroids.size() << endl;
+    // cout << centroids << endl;
+    // cout << values.size() << endl;
+    // cout << values << endl;
+    // waitKey(0);
+    Mat res = interpolate(centroids, values, blockSize);
+    // resize(res, res, src1.size(), 0, 0, INTER_LINEAR);
     return res;
   }
 }
